649abba7b7556ee7a957e93207fb3ecdd66f402b
HouseofAloha <jay@houseofaloha.com.au>
2020-06-21 17:57:40 +1000

    Updates

---
+++ precache-manifest.3350e4abb293006a5cb831c054c333bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbf0d35af05415bb8e1b64420397d6b2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7f59c77ca25f9fcc9805",
+    "url": "/monsters-rolodex/static/css/main.4e1474fc.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f59c77ca25f9fcc9805",
+    "url": "/monsters-rolodex/static/js/main.5c3d479a.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
